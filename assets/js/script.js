f5df5626e730fb203b50e1046e8ef4433ceea06d
unknown <davidstew87@gmail.com>
2022-10-27 20:47:24 -0400

    both api's working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,29 @@
-fetch('https://app.ticketmaster.com/discovery/v2/events.json?apikey=xdshV4xA76PjaILVNZCQLJMfvKXyiWU8')
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  });
+const Tapi_key = "xdshV4xA76PjaILVNZCQLJMfvKXyiWU8"
+const Yapi_key = "joPjHokXrrhul1ER2zqKLt-tNwjjmYmgrBQTbNHd88SXmuSquMbPyGMELmNDno0XosGjL8sHvOd6R7Ac2uRmeTCKUTloVAWezetEkmaRbsYiXB1VYVMFmQdjhRlbY3Yx"
+var cityformEl = document.getElementById("city-form")
+ cityformEl.addEventListener("submit", (event) => {
+    var cityInput = document.getElementById('citySelection').value;
+    event.preventDefault();
+
+    fetch(`https://app.ticketmaster.com/discovery/v2/events.json?apikey=${Tapi_key}`)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+  
+    fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=${cityInput}`,{
+        headers:{
+            authorization: "Bearer " + Yapi_key
+        }
+    })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+   
+})
+
